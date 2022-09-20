688c529df0a3818db5f7796ce118f4a98fce4514
Jonatas Carvalho <jonatas.carvalho@xrtbrasil.adinternal.com.br>
2022-09-20 19:58:27 -0300

    't'

--- BHub/settings.py
+++ BHub/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'BHub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        dj_database_url.config()
+#    }
+#}
+
 DATABASES = {
     'default': {
-        dj_database_url.config()
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
